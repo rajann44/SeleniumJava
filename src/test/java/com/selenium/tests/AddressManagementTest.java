d96aca8eb68bb48cf7b8258e04dda6cadb2bc63a
rajann44 <rajann44@gmail.com>
2021-01-10 18:16:00 +0100

    Add AddressManagementTest

---
+++ src/test/java/com/selenium/tests/AddressManagementTest.java
@@ -0,0 +1,27 @@
+package com.selenium.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+
+public class AddressManagementTest {
+    private WebDriver driver;
+    
+    @BeforeClass
+    public void setUp() {
+        driver = new ChromeDriver();
+    }
+    
+    @Test
+    public void testScenario() {
+        // Test implementation
+    }
+    
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
