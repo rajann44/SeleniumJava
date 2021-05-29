12ab412974feb607d76759da3491f398303e4598
rajann44 <rajann44@gmail.com>
2021-05-29 14:48:30 +0530

    removed the comment

--- src/test/java/testScripts/VerifySignUp.java
+++ src/test/java/testScripts/VerifySignUp.java
@@ -16,7 +16,7 @@ public class VerifySignUp {
         signupPage.navigateToTheURL("https://google.com/");
         signupPage.navigateToTheURL("https://amazon.com");
         signupPage.navigateToTheURL("https://amazon.com/");
-        //signupPage.sleepFor(5);
+        signupPage.sleepFor(5);
         signupPage.refreshBrowser();
 
     }
