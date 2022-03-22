18d9b72cc5b12cdd3a5bdf3ef065794121a9c352
Rajan Chaudhary <60035342+rajann44@users.noreply.github.com>
2022-03-23 02:47:06 +0530

    add tests

--- src/test/java/testScripts/VerifySignUp.java
+++ src/test/java/testScripts/VerifySignUp.java
@@ -11,17 +11,35 @@ public class VerifySignUp {
 
     SignupPage signupPage = new SignupPage();
 
-    @Test
-    public void justBrowser(){
-
-        signupPage.navigateToTheURL("https://google.com/");
-        signupPage.navigateToTheURL("https://google.com/");
-        signupPage.navigateToTheURL("https://google.com/");
-        signupPage.navigateToTheURL("https://google.com/");
+    @Test(priority = 1)
+    public void navigation_01(){
         signupPage.navigateToTheURL("https://google.com/");
         Assert.assertTrue(true);
-        signupPage.refreshBrowser();
+    }
 
+    @Test(priority = 2)
+    public void navigation_02(){
+        signupPage.navigateToTheURL("https://bing.com/");
+        Assert.assertTrue(true);
+        signupPage.refreshBrowser();
+    }
+
+    @Test(priority = 3)
+    public void navigation_03(){
+        signupPage.navigateToTheURL("https://www.sony.com/");
+        Assert.assertTrue(true);
+    }
+
+    @Test(priority = 4)
+    public void navigation_04(){
+        signupPage.navigateToTheURL("http://yahoo.com/");
+        Assert.assertTrue(true);
+    }
+
+    @Test(priority = 5)
+    public void navigation_05(){
+        signupPage.navigateToTheURL("https://www.aol.com/");
+        Assert.assertTrue(true);
     }
 
 }
